b3ad503a78c18b16a6bf6b46cb4f853d75d1c29a
ignacio tosini <ignaciotosini2002@gmail.com>
2026-02-16 20:41:40 -0300

    cambio https://hekademos.vercel.app

--- src/main/java/com/hekademos/hekademos_backend/controllers/ExerciseController.java
+++ src/main/java/com/hekademos/hekademos_backend/controllers/ExerciseController.java
@@ -32,7 +32,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 @RequestMapping("/api/exercises")
-@CrossOrigin(origins = { "http://localhost:5173", "http://127.0.0.1:5173", "https://hekademos-frontend.vercel.app" })
+@CrossOrigin(origins = { "http://localhost:5173", "http://127.0.0.1:5173", "https://hekademos.vercel.app" })
 public class ExerciseController {
     @Autowired
     private IExerciseService exerciseService;
--- src/main/java/com/hekademos/hekademos_backend/controllers/UserController.java
+++ src/main/java/com/hekademos/hekademos_backend/controllers/UserController.java
@@ -18,7 +18,7 @@ import com.hekademos.hekademos_backend.services.IUserService;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = { "http://localhost:5173", "http://127.0.0.1:5173", "https://hekademos-frontend.vercel.app" })
+@CrossOrigin(origins = { "http://localhost:5173", "http://127.0.0.1:5173", "https://hekademos.vercel.app" })
 public class UserController {
     @Autowired
     private IUserService userService;
